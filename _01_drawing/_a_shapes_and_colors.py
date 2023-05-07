bae62586adaa38284ff96d2b8b57679c577ea485
echohunter <eddiejhunter1000000@gmail.com>
2023-05-07 13:11:29 -0700

    shapes and colors

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,18 +5,27 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+    Chunky_Guy = turtle.Turtle()
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
+    Chunky_Guy.shape("turtle")
     # Set your turtle's speed using .speed(2)
-
+    Chunky_Guy.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    Chunky_Guy.color('lime')
     # Move your turtle forward using .forward(100)
     # TEST    Did your turtle move forward?
-
+    Chunky_Guy.begin_fill()
+    Chunky_Guy.forward(150)
     # Move your turtle left or right using .left(90) or .right(90)
+    for i in range(100):
+        Chunky_Guy.left(90)
+        Chunky_Guy.forward(150)
+        Chunky_Guy.left(90)
+        Chunky_Guy.forward(150)
+        Chunky_Guy.left(90)
+        Chunky_Guy.forward(150)
+        Chunky_Guy.end_fill()
 
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
@@ -34,3 +43,4 @@ if __name__ == '__main__':
 
     # ===================== DO NOT EDIT THE CODE BELOW ============================
     turtle.done()
+
